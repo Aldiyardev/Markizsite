b28d32b8983f46384d451ca163390a057cdb56ba
Aldia <aldiyarkabylbek@gmail.com>
2026-07-28 17:26:48 +0800

    first commit

---
+++ appshi.js
@@ -0,0 +1,14 @@
+
+  const hamburger = document.querySelector(".hamburger");
+  const nav_menu = document.getElementById("nav-menu");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    nav_menu.classList.toggle("active");
+  })
+
+  document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    nav_menu.classList.remove("active");
+  }))
+
